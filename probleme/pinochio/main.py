94d5a7becfa12c393d19b606af6225eb1c0fc163
Radu Mirescu <radumirescu4@gmail.com>
2020-11-12 19:48:13 +0200

    adaugat suport pentru teste automate

--- probleme/pinochio/main.py
+++ probleme/pinochio/main.py
@@ -1,4 +1,12 @@
+import unittest  # import biblioteca pentru teste unitare
+
+
+########################################################################################################################
+# DE AICI INCEPE PARTEA DE TESTARE AUTOMATA
+class ScriptTests(unittest.TestCase):  # adaug clasa care va contine testele
+    def test1(self):                   # cu un prim test "fake" care trece 
+        self.assertTrue(True)
 
 
 if __name__ == "__main__":
-    print("Mod test")
+    unittest.main()
