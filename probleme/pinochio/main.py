82b0ad5de1621f9f27aaf2d577da1295ab17430e
Radu Mirescu <radumirescu4@gmail.com>
2020-11-12 20:33:29 +0200

    test generic rezolvat

--- probleme/pinochio/main.py
+++ probleme/pinochio/main.py
@@ -1,15 +1,42 @@
 import unittest  # import biblioteca pentru teste unitare
+import os
 
 
 def lungime_nas_pinochio(lungime_initiala_nas, rata_crestere, numar_de_zile):
-    return 0  # lungimea nasului lui Pinochio dupa numar_de_zile
+    return 0  # lungimea nasului lui Pinochio la sfarsit
 
 
 ########################################################################################################################
 # DE AICI INCEPE PARTEA DE TESTARE AUTOMATA
 class ScriptTests(unittest.TestCase):  # adaug clasa care va contine testele
-    def test1(self):                   # cu un prim test "fake" care trece
-        self.assertTrue(True)
+    def _test_generic(self, nume_test):
+        # obtin continutul fisierului
+        cale_fisier = f"{os.path.dirname(os.path.realpath(__file__))}/{nume_test}.txt"
+        with open(cale_fisier, "r") as f:
+            file_content = f.read()
+
+        # ... si il spart in parti
+        parti_fisier = file_content.split("=====")
+
+        # ... din care iau date intrare
+        date_intrare = parti_fisier[0].strip()
+        # ... si rezultat (care trebuie sa fie un int)
+        rezultat_asteptat = int(parti_fisier[1].strip())
+
+        # separ datele de intrare cu spatiu si obtin parametrii de apel
+        lista_date_intrare = date_intrare.split(" ")
+        lungime_initiala_nas = int(lista_date_intrare[0])
+        rata_crestere_nas = int(lista_date_intrare[1])
+        perioada = int(lista_date_intrare[2])
+
+        # apelez functia principala si obtin rezultatul real
+        rezultat_real = lungime_nas_pinochio(lungime_initiala_nas, rata_crestere_nas, perioada)
+
+        # ... pe care-l compar cu rezultatul asteptat
+        self.assertEqual(rezultat_asteptat, rezultat_real)
+
+    def test1(self):
+        self._test_generic("test1")
 
 
 if __name__ == "__main__":
