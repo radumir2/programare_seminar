4765fd9b0576b0d2cdf85179c980133bde078913
Radu Mirescu <radumirescu4@gmail.com>
2020-11-12 20:06:13 +0200

    am definit functia care rezolva problema

--- probleme/pinochio/main.py
+++ probleme/pinochio/main.py
@@ -1,6 +1,10 @@
 import unittest  # import biblioteca pentru teste unitare
 
 
+def lungime_nas_pinochio(lungime_initiala_nas, rata_crestere, numar_de_zile):
+    return 0  # lungimea nasului lui Pinochio dupa numar_de_zile
+
+
 ########################################################################################################################
 # DE AICI INCEPE PARTEA DE TESTARE AUTOMATA
 class ScriptTests(unittest.TestCase):  # adaug clasa care va contine testele
